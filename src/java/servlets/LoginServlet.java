8f5e40c07b85e78391e6974c155d597c634426b3
Chris <Chris@192.168.4.208>
2023-02-11 15:15:30 -0700

    Week5 lab mylogin creation

---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Christian
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+        }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+}
---
+++ web/WEB-INF/login.jsp
@@ -0,0 +1,28 @@
+<%-- 
+    Document   : login
+    Created on : 11-Feb-2023, 2:55:42 PM
+    Author     : Chris
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <<form action="action" method="">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="text" name="password"><br>
+            
+            
+        </form> 
+       
+       
+       
+       
+        
+    </body>
+</html>
